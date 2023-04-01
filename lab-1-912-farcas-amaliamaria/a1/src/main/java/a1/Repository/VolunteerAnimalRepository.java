7c3da8b8932754ad2fde2ae85aaab2a3978984ac
912-farcas-amaliamaria <amalia.farcas@stud.ubbcluj.ro>
2023-04-01 20:38:56 +0300

    just the FE

--- lab-1-912-farcas-amaliamaria/a1/src/main/java/a1/Repository/VolunteerAnimalRepository.java
+++
@@ -1,7 +0,0 @@
-package a1.Repository;
-
-import a1.Domain.VolunteerAnimal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VolunteerAnimalRepository extends JpaRepository<VolunteerAnimal, Long> {
-}
--- lab-1-912-farcas-amaliamaria/a1/src/main/java/a1/Service/IVolunteerService.java
+++
@@ -1,4 +0,0 @@
-package a1.Service;
-
-public interface IVolunteerService {
-}
